a1f6b8bbadfa0b56aef3f542298b21e0b0f2cfa1
Jassem Al-Buloushi <jassem1988@gmail.com>
2019-04-06 15:45:29 +0300

    Append country names to countries array

--- Guess_The_Flag/ViewController.swift
+++ Guess_The_Flag/ViewController.swift
@@ -18,7 +18,9 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        countries += ["estonia","france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
     }
 
 
